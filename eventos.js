b8cca63b6efeab64d150a8d96c75a828b9220100
kevin259819 <bitancortkevin9@gmail.com>
2025-09-11 14:15:14 -0300

    aparece el div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+addEventListener('DOMContentLoaded', function() {
+    let divs = document.querySelector('div');
+    divs.addEventListener('click', function() {
+        alert('Hola! soy el div');
+    });
+});
